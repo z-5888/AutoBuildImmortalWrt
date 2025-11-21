d6ef7348a6ac68fc698af7ed964735a7f6bb262e
z-5888 <104440537+z-5888@users.noreply.github.com>
2025-11-21 13:49:59 +0800

    Update build23.sh

--- rockchip/build23.sh
+++ rockchip/build23.sh
@@ -61,6 +61,13 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+
+PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
+PACKAGES="$PACKAGES luci-app-xlnetacc"
+
+
+
+
 # 代理工具
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
